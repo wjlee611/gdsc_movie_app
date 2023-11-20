714ecd4401a5585525c34aec805e458de34dd418
wjlee611 <wjlee611@gmail.com>
2023-11-20 14:29:11 +0900

    add enum value - title

--- lib/enums/tmdb_movie_list_type.dart
+++ lib/enums/tmdb_movie_list_type.dart
@@ -1,12 +1,16 @@
 enum TMDBMovieListType {
-  nowPlaying('now_playing'),
-  popular('popular'),
-  topRated('top_rated'),
-  upComing('upcoming'),
-  day('day'),
-  week('week');
+  nowPlaying('now_playing', '현재 상영 중'),
+  popular('popular', '인기 영화'),
+  topRated('top_rated', '높은 평점 영화'),
+  upComing('upcoming', '상영 예정'),
+  day('day', '일간 트렌드'),
+  week('week', '주간 트렌드');
 
   final String endPoint;
+  final String title;
 
-  const TMDBMovieListType(this.endPoint);
+  const TMDBMovieListType(
+    this.endPoint,
+    this.title,
+  );
 }
